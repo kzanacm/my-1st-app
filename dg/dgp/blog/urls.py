ecba1bd2cf9dc0ac4561ce77b10ebd543d1d5097
kzanacm <kzanacm@gmail.com>
2021-04-18 20:10:26 -0300

    detalhe.

--- dg/dgp/blog/urls.py
+++ dg/dgp/blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.postagem_lista, name='hpostagem_lista'),
+    path('postagem_detalhe/<int:pk>/', views.postagem_detalhe, name='hpostagem_detalhe'),
 ]
--- dg/dgp/blog/views.py
+++ dg/dgp/blog/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Postagem
 
 
 def postagem_lista(request):
-    Postagens = Postagem.objects.filter(ptg_ts_criacao__lte=timezone.now()).order_by('ptg_ts_criacao') 
-    return render(request, 'blog/postagem_lista.html', {'Postagens': Postagens})
+    postagens = Postagem.objects.filter(ptg_ts_criacao__lte=timezone.now()).order_by('ptg_ts_criacao') 
+    return render(request, 'blog/postagem_lista.html', {'postagens': postagens})
+
+
+def postagem_detalhe(request, pk):
+    postagem = get_object_or_404(Postagem, pk=pk)
+    return render(request, 'blog/postagem_detalhe.html', {'postagem': postagem})
