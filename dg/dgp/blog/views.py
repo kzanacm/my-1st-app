db0814b926061f96877cf3a97d03d2f640a0e029
kzanacm <kzanacm@gmail.com>
2021-04-18 17:55:16 -0300

    Modified templates to display posts from database.

--- dg/dgp/blog/views.py
+++ dg/dgp/blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Postagem
 
 
 def postagem_lista(request):
-    return render(request, 'blog/postagem_lista.html', {})
+    Postagens = Postagem.objects.filter(ptg_ts_criacao__lte=timezone.now()).order_by('ptg_ts_criacao') 
+    return render(request, 'blog/postagem_lista.html', {'Postagens': Postagens})
